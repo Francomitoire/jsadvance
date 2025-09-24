0fd3ca8f6f3eb580f3a14e0f1d1040307595ea6d
Ivan Gauto <ivan.gauto@gm2dev.com>
2025-09-24 10:51:57 -0300

    feat/vehicle function and metods

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-let globalVar = "Esto es una variable global";
+// let globalVar = "Esto es una variable global";
 
-function myFunction() {
-  let functionVar = "Dentro de la funcion";
-  if (true) {
-    let blockVar = "Dentro de un block";
-    console.log(blockVar);
-  }
-  console.log(functionVar);
+// function myFunction() {
+//   let functionVar = "Dentro de la funcion";
+//   if (true) {
+//     let blockVar = "Dentro de un block";
+//     console.log(blockVar);
+//   }
+//   console.log(functionVar);
+// }
+// myFunction();
+
+function vehiculo(marca, modelo, velocidadMaxima) {
+  this.marca = marca;
+  this.modelo = modelo;
+  this.velocidadMaxima = velocidadMaxima;
+  this.velocidadActual = 0;
 }
-myFunction();
+
+vehiculo.prototype.acelerar = function (velocidad) {
+  this.velocidadActual += velocidad;
+};
+
+vehiculo.prototype.detener = function () {
+  this.velocidadActual = 0;
+};
+
+const vehiculo1 = new vehiculo("toyota", "hilux", 170);
+vehiculo1.acelerar(70);
+console.log(vehiculo1.velocidadActual);
+vehiculo1.detener();
+console.log(vehiculo1.velocidadActual);
