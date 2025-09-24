734cc65c25b21ed9917053ed9b7558b8444b5e48
Diego Croquer <diego.croquer@gm2dev.com>
2025-09-24 11:17:25 -0300

    feat: add class Car

--- diegoScript.js
+++ diegoScript.js
@@ -1,4 +1,4 @@
-class vehicle {
+class Vehicle {
   constructor(marca, modelo, velocidad, velocidadMaxima) {
     this.marca = marca;
     this.modelo = modelo;
@@ -7,7 +7,7 @@ class vehicle {
   }
 }
 
-vehicle.prototype.acelerar = function (velocidad) {
+Vehicle.prototype.acelerar = function (velocidad) {
   if (this.velocidad + velocidad <= this.velocidadMaxima) {
     this.velocidad += velocidad;
   } else {
@@ -15,16 +15,32 @@ vehicle.prototype.acelerar = function (velocidad) {
   }
 };
 
-vehicle.prototype.detener = function () {
+Vehicle.prototype.detener = function () {
   this.velocidad = 0;
 };
 
-vehicle.prototype.informacion = function () {
+Vehicle.prototype.informacion = function () {
   return `Marca: ${this.marca}\nModelo: ${this.modelo}\nVelocidad: ${this.velocidad} km/h\nVelocidad Maxima: ${this.velocidadMaxima} km/h`;
 };
 
-const miCoche = new vehicle("Toyota", "Corolla", 0, 180);
+const miCoche = new Vehicle("Toyota", "Corolla", 0, 180);
+
 miCoche.acelerar(150);
 console.log(miCoche.informacion());
 miCoche.detener();
 console.log(miCoche.informacion());
+
+class Car extends Vehicle {
+  constructor(marca, modelo, velocidad, velocidadMaxima, openDoor) {
+    this.openDoor = openDoor;
+    super(marca, modelo, velocidad, velocidadMaxima);
+  }
+}
+
+Car.prototype.openDoor = function () {
+  if (this.openDoor) {
+    console.log("La puerta del auto está abierta.");
+  } else {
+    console.log("La puerta del auto está cerrada.");
+  }
+};
