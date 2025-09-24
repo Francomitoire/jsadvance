edc38ef4fbdad25e3b058c0ddf91b6e224693f7f
Ivan Gauto <ivan.gauto@gm2dev.com>
2025-09-24 10:28:11 -0300

    Cambrio previo al revert

--- script.js
+++ script.js
@@ -9,14 +9,3 @@ function myFunction() {
   console.log(functionVar);
 }
 myFunction();
-
-function vehiculo(marca, modelo, velocidadMaxima) {
-  this.marca = marca;
-  this.modelo = modelo;
-  this.velocidadMaxima = velocidadMaxima;
-  this.velocidadActual = 0;
-}
-
-vehiculo.prototype.acelerar = function (velocidad) {
-  this.velocidadActual += velocidad;
-};
