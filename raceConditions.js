dea13718bcdff822b64d88177359cc814d293895
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-09-25 11:36:20 -0300

    feat(raceConditions.js): create simulation structure

--- raceConditions.js
+++ raceConditions.js
@@ -1,4 +1,16 @@
 // MATI WORK
+function runSimulation(){
+    const usuarios = []
+    const entradas = []
+    const tickets = []
+    for(let i = 1; i <= 15; i++){
+        usuarios.push(`usuario${i}`)
+        if(i<=10){
+            entradas.push(`entrada${i}`)
+        }
+    }
+}
+runSimulation()
 // DAVID WORK
 // IVAN WORK
 // ALEX WORK
