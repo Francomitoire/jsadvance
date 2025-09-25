b8e95a28700f114c045919f3c8cad4fcd5f85517
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-09-25 11:37:22 -0300

    feat(raceConditions.js): add sacarEntradas() functions with promise handling

--- raceConditions.js
+++ raceConditions.js
@@ -9,6 +9,30 @@ function runSimulation(){
             entradas.push(`entrada${i}`)
         }
     }
+    
+    const sacarEntradas = async ()=>{
+        const promesasEntradas = usuarios.map((usuario)=>{
+            return new Promise((resolve, reject)=>{
+                setTimeout(()=>{
+                    if(entradas.length !== 0){
+                        const cantidadEntradas = Math.floor(Math.random() * entradas.length) + 1
+                        const ticket = {
+                            nombre: usuario,
+                            entradasUsuario : []
+                        }
+                        for(let i = 0; i < cantidadEntradas; i++){
+                            ticket.entradasUsuario.push(entradas.shift())
+                        }
+
+                        resolve(ticket)
+                    }else{
+                        reject(`Lo siento ${usuario}, se han acabado las entradas`)
+                    }
+                }, Math.random() * 5000)
+            })
+        })
+    }
+    sacarEntradas()
 }
 runSimulation()
 // DAVID WORK
