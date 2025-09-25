896a01f07fb0d161fe17c9c943f4139267c14858
Diego Croquer <diego.croquer@gm2dev.com>
2025-09-25 10:38:43 -0300

    feat: add funcion sellTickets

--- raceConditions.js
+++ raceConditions.js
@@ -3,3 +3,24 @@
 // IVAN WORK
 // ALEX WORK
 // DIEGO WORK
+
+let tickets = 10; //tickets disponibles
+let requests = 0; //tickets solicitados
+let ticketsSold = 0; //tickets vendidos
+
+//funcion para vender tickets
+function sellTicket(requests) {
+  if (requests <= tickets) {
+    ticketsSold += requests;
+    tickets -= requests;
+  } else {
+    console.log("No hay mas tickets disponibles");
+  }
+}
+
+console.log("1", ticketsSold);
+console.log("2", tickets);
+sellTicket(3); //3 tickets solicitados
+console.log("3", ticketsSold);
+console.log("4", tickets);
+//
