0a8e3f8b4586c9ce81d13f9a00d9cf563b718647
Alex <alex.gioiosa@gm2dev.com>
2025-09-26 11:39:23 -0300

    feat: create buyTicket function

--- raceConditions.js
+++ raceConditions.js
@@ -2,4 +2,41 @@
 // DAVID WORK
 // IVAN WORK
 // ALEX WORK
+function runSimulation() {
+  let tickets = 10;
+  let users = [
+    "Alex",
+    "Matias",
+    "Diego",
+    "Ivan",
+    "Franco",
+    "Juan",
+    "David",
+    "Doyel",
+    "Piero",
+    "Nacho",
+    "Korzu",
+    "Carolina",
+  ];
+  function buyTicket() {
+    users.forEach((user) => {
+      setTimeout(() => {
+        console.log(user);
+      }, Math.random() * 5000);
+      if (tickets <= 0) {
+        console.log("There are not more tickets");
+      } else {
+        console.log(`There are ${AcaVaAlgo}, you have bought`);
+      }
+      if (user) {
+        function tryToBuyATicket() {
+          `${user} has bought a ticket ${(tickets = tickets - 1)}`;
+          tickets < 10;
+        }
+        tryToBuyATicket();
+      }
+    });
+  }
+  buyTicket();
+}
 // DIEGO WORK
